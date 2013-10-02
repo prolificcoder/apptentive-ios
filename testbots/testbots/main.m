cd91ceea6b26b8e9b7b3dc0520e0cc7f02d760a8
Satyajit Malugu <satyajitmalugu@server.local>
2013-10-02 14:12:35 -0700

    scheme required by Bot

---
+++ testbots/testbots/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testbots
+//
+//  Created by Satyajit Malugu on 10/2/13.
+//  Copyright (c) 2013 Satyajit Malugu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
